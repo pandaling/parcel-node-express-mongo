b397813a7617807aca9d4e64b0ef9c683d84290d
Ling <cs_ling@mapit.com.my>
2019-07-27 12:05:39 +0800

    + first commit. + added setup files.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # parcel-node-express-mongo
 A basic starter for a web app with parceljs, node, express and mongoose
+
+## Pre-start
+	- npm
+	- nodemon installed globally in your laptop
+
+## Features
+	- Babel 7
+	- Express
+	- Bunyan
+	- Pug
+
+## How to start
+	- git clone `https://github.com/pandaling/parcel-node-express-mongo.git`
+	- cd `parcel-node-express-mongo`
+	- npm install
+	- npm run server
+	- open browser to `localhost:8000` (port default to 8000)
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import helmet from 'helmet';
+import http from 'http';
+import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import log from '../util/logger';
+
+/**
+  * Create express server
+  */
+const app = express();
+
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'pug');
+app.use(helmet());
+app.use(cors());
+// get information from html forms
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Welcome Page', welcomeMsg: 'You are welcome.' });
+});
+
+/**
+  * Start Express server
+  */
+const server = http.createServer(app);
+server.listen(app.get('port'), () => {
+  log.info('Server is running at port %s', app.get('port'));
+});
